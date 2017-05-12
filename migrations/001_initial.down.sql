af15d917dea7e8850f3af1c8797645a154cc2a15
Alex Macleod <alex@macleod.io>
2017-05-12 09:50:50 +0100

    replace enum with int

--- migrations/001_initial.down.sql
+++ migrations/001_initial.down.sql
@@ -1,3 +1,2 @@
 DROP TABLE devices;
 DROP TABLE users;
-DROP TYPE DEVICE_TYPE;
--- migrations/001_initial.up.sql
+++ migrations/001_initial.up.sql
@@ -2,13 +2,11 @@ CREATE TABLE users (
   id INT PRIMARY KEY
 );
 
-CREATE TYPE DEVICE_TYPE AS ENUM ('chrome', 'android');
-
 CREATE TABLE devices (
   id          INT PRIMARY KEY,
 
   name        VARCHAR(80) NOT NULL,
-  device_type DEVICE_TYPE NOT NULL,
+  device_type INT         NOT NULL,
 
   user_id     INT         NOT NULL REFERENCES users
 );

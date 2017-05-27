8dbf3b9cc79efc05a35de0dd5b0047cf3d1c0cd4
Alex Macleod <alex@macleod.io>
2017-05-27 13:51:42 +0100

    free up invite keys when no connections are present

--- routes/invite.go
+++ routes/invite.go
@@ -4,10 +4,61 @@ import (
 	"io"
 	"net/http"
 
+	"sync"
+
+	"log"
+
 	"github.com/gorilla/mux"
 )
 
+type Invites struct {
+	sync.RWMutex
+	invites map[string]*invite
+}
+
+func NewInvites() *Invites {
+	return &Invites{
+		invites: make(map[string]*invite),
+	}
+}
+
+// Get or create an invite
+func (i *Invites) Get(key string) *invite {
+	i.RLock()
+	inv, ok := i.invites[key]
+	i.RUnlock()
+
+	if !ok {
+
+		inv = &invite{
+			sync.WaitGroup{},
+			make(chan *http.Request),
+			make(chan http.ResponseWriter),
+			make(chan *http.Request),
+			make(chan http.ResponseWriter),
+		}
+
+		i.Lock()
+		i.invites[key] = inv
+		i.Unlock()
+
+		inv.wg.Add(1)
+
+		go func() {
+			inv.wg.Wait()
+			log.Printf("invite key freed: %v", key)
+			delete(i.invites, key)
+		}()
+	} else {
+		inv.wg.Add(1)
+	}
+
+	return inv
+}
+
 type invite struct {
+	wg sync.WaitGroup
+
 	srcReq chan *http.Request
 	srcRes chan http.ResponseWriter
 
@@ -15,16 +66,7 @@ type invite struct {
 	destRes chan http.ResponseWriter
 }
 
-func newInvite() *invite {
-	return &invite{
-		make(chan *http.Request),
-		make(chan http.ResponseWriter),
-		make(chan *http.Request),
-		make(chan http.ResponseWriter),
-	}
-}
-
-var invites = make(map[string]*invite)
+var invites = NewInvites()
 
 func InviteGet(w http.ResponseWriter, r *http.Request) {
 	vars := mux.Vars(r)
@@ -32,12 +74,9 @@ func InviteGet(w http.ResponseWriter, r *http.Request) {
 	key := vars["key"]
 	deviceType := vars["device"]
 
-	inv, ok := invites[key]
-
-	if !ok {
-		inv = newInvite()
-		invites[key] = inv
-	}
+	inv := invites.Get(key)
+	defer inv.wg.Done()
+	defer log.Println("req done")
 
 	var err error
 	if deviceType == "src" {
@@ -58,12 +97,9 @@ func InvitePost(w http.ResponseWriter, r *http.Request) {
 	key := vars["key"]
 	deviceType := vars["device"]
 
-	inv, ok := invites[key]
-
-	if !ok {
-		inv = newInvite()
-		invites[key] = inv
-	}
+	inv := invites.Get(key)
+	defer inv.wg.Done()
+	defer log.Println("post done")
 
 	var err error
 	if deviceType == "src" {
